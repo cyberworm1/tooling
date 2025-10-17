f2326de4333e123cf32fe4a6fa2838f6511e338f
cyberworm1 <cyberworm@gmail.com>
2025-10-17 10:40:08 -0400

    Add documentation and improve cloud cost tooling

--- terraform/azure/storageProvision/main.tf
+++ terraform/azure/storageProvision/main.tf
@@ -3,37 +3,47 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "media-assets-rg"
-  location = "East US"
-}
-
-resource "azurerm_storage_account" "storage" {
-  name                     = "gameassetsstorage${random_string.random.id}"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"  # Geo-redundant for media IP protection
-  enable_https_traffic_only = true
-  min_tls_version          = "TLS1_2"
-  allow_nested_items_to_be_public = false  # Enforce private access
-
-  blob_properties {
-    delete_retention_policy {
-      days = 7  # Soft-delete for accidental asset loss
-    }
-  }
-}
-
-resource "azurerm_storage_container" "container" {
-  name                  = "game-assets"
-  storage_account_name  = azurerm_storage_account.storage.name
-  container_access_type = "private"
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
 }
 
 resource "random_string" "random" {
   length  = 6
   special = false
   upper   = false
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "gameassets${random_string.random.id}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = var.account_tier
+  account_replication_type = var.replication_type
+  enable_https_traffic_only       = true
+  min_tls_version                 = "TLS1_2"
+  allow_nested_items_to_be_public = false
+  tags                            = var.tags
+
+  blob_properties {
+    delete_retention_policy {
+      days = 7
+    }
+  }
+
+  dynamic "network_rules" {
+    for_each = length(var.allowed_ip_rules) > 0 ? [1] : []
+    content {
+      default_action = "Deny"
+      ip_rules       = var.allowed_ip_rules
+    }
+  }
+}
+
+resource "azurerm_storage_container" "container" {
+  name                  = var.container_name
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "private"
 }
 
 output "storage_account_name" {
